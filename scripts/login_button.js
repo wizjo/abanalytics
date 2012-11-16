a5ded9a920a814bf0468d0db8235ef4ee5a5751a
Jo Pu <zpu@yammer-inc.com>
2012-11-16 09:23:29 -0800

    More stuff.

--- scripts/login_button.js
+++ scripts/login_button.js
@@ -1,6 +1,55 @@
+var App = App || {};
+
+App.endpoints = App.endpoints || {
+// This week's aniversary
+  anniversary: "http://stageanalytics.int.yammer.com/queries/33/query_executions/638.json?refresh=true",
+
+// All Yammer staging's active users joined thread
+  joined: "http://stageanalytics.int.yammer.com/queries/33/executions/1640.json",
+
+// Network champions on MSFT
+  msft_champions: "http://analytics.int.yammer.com/queries/535/executions/56834.json",
+
+// Network champions on yammer staging
+  staging_champions: "http://stageanalytics.int.yammer.com/queries/33/executions/1648.json",
+
+// Thread stats
+  thread: function(thread_id) {
+    return("https://www.staging.yammer.com/api/v1/threads/" + thread_id + ".json");
+// web_url: https://www.staging.yammer.com/yammer-inc.com#/Threads/show?threadId=[:thread_id]
+  },
+
+// User profile
+  user: function(user_id) {
+    return("https://www.staging.yammer.com/api/v1/users/" + user_id + ".json");
+  },
+
+// Current user
+  current_user: "https://www.staging.yammer.com/api/v1/users/current.json"
+};
+
+yam.config({ appId: "TvYDhcTzTPco3DYkDFtDVw" });
+
 yam.connect.loginButton('#yammer-login', function (resp) {
   if(resp.authResponse) {
-    document.getElementById('yammer-login').innerHTML = 'Welcome to Yammer, ' + resp.user.full_name + "!";
+    // fix for Chrome, when initial authentication failed
+    if(resp.user === undefined) {
+      console.log("Authentication failed!")
+      window.location.href = "/";
+    } else{
+      // Authentication passed. Update template and fire events.
+      App.user = { full_name: resp.user.full_name,
+        user_id: resp.user.url.match(/\d+$/)[0],
+        web_url: resp.user.web_url
+      };
+
+      $("#yammer-login").html("");
+
+      var source = $("#greeting").html();
+      var template = Handlebars.compile(source);
+      var result = template(App.user);
+      $("#top-info").html(result);
+
+    }
   }
 });
-
---
+++ scripts/storyboard.js
@@ -0,0 +1,23 @@
+$.getJSON(App.endpoints.staging_champions, function(resp) {
+  //console.log(resp);
+  App.data = App.data || {};
+  App.data.champions = resp;
+
+  var circle = new Path.Circle(new Point(80, 50), 35);
+  circle.fillColor = 'red';
+
+  var circle2 = new Path.Circle(new Point(120, 50), 35);
+  circle2.style = {
+      fillColor: 'blue',
+      strokeColor: 'green',
+      strokeWidth: 10
+  };
+
+  // Make circle2 50% transparent:
+  circle2.opacity = 0.5;
+  // Create a centered text item at the center of the view:
+  var text = new PointText(view.center);
+  text.paragraphStyle.justification = 'center';
+  text.characterStyle.fontSize = 20;
+  text.fillColor = 'white';
+});
